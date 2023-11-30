68db99f58ce60e9f52422c8a4efa12c7f0df6a8a
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2023-11-30 20:16:50 +0100

    add get endpoint

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+from flask import jsonify
+from flask import request
+from flask import Response
 
 app = Flask(__name__)
 
+users = []
+
 
 @app.get("/users")
-def get_users() ->
+def get_users() -> Response:
+    return jsonify(users)
