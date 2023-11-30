3925ce6c78d7aac81db0e847c29b2c81ebcd3ab6
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2023-11-30 20:53:31 +0100

    dodanie endpointow + jakis szkic calosci

--- src/app.py
+++ src/app.py
@@ -10,4 +10,29 @@ users = []
 
 @app.get("/users")
 def get_users() -> Response:
+    return jsonify(users, status=200)
+
+
+@app.get("/users/")
+def get_one_user() -> Response(status=200):
     return jsonify(users)
+
+
+@app.post("/users")
+def create_user() -> Response:
+    body = request.json
+    users.append(body)
+    return Response(status=201)
+
+
+@app.patch("/users/")
+def update_user() -> Response:
+
+
+@app.delete("/users/")
+def delete_user() -> Response:
+    return Response(status=204)
+
+
+if __name__ == "__main__":
+    app.run("localhost", 8080, debug=True)
