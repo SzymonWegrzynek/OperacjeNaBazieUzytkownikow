3b5510942b2eac35ee6efd2a919f45c3882c200c
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2023-12-01 08:47:10 +0100

    add integration test

--- test/integration/test_app.py
+++ test/integration/test_app.py
@@ -4,9 +4,15 @@ STATUS_OK = 200
 CREATE = 204
 
 
-def test_ping_endpoint() -> None:
+def test_users_endpoint() -> None:
     client = app.test_client()
-    actual = client.get("/ping")
+    actual = client.get("/users")
+    assert actual.status_code == STATUS_OK
+
+
+def test_get_one_user_endpoint() -> None:
+    client = app.test_client()
+    actual = client.get("/users/1")
     assert actual.status_code == STATUS_OK
 
 
@@ -14,3 +20,20 @@ def test_create_user_endpoint() -> None:
     client = app.test_client()
     actual = client.post("/users")
     assert actual.status_code == CREATE
+
+
+def test_patch_user_endpoint(status_code=None) -> None:
+    data = {'id': 1, 'name': 'Updated Name'}
+    response_patch = app.patch('/users', json=data)
+    assert (response_patch, status_code == 204)
+
+
+def test_put_user_endpoint(status_code=None) -> None:
+    data = {'id': 1, 'name': 'Replaced Name'}
+    response_put = app.put('/users/1', json=data)
+    assert (response_put, status_code == 204)
+
+
+def test_delete_user_endpoint(status_code=None) -> None:
+    data = app.delete('/users/1')
+    assert (data, status_code == 204)
--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -20,3 +20,9 @@ def test_returns_400_on_bad_request() -> None:
     with app.test_request_context(json=payload):
         actual = create_user()
     assert actual.status_code == 400
+
+
+def test_get_users(self):
+    response = self.app.get('/users')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json, self.users)
